34cac0d2327384d3e49f3ba42e1f8da9b47db7ed
Aidan <ams148@email.latech.edu>
2025-01-09 12:52:00 -0600

    Update cli.js

--- totp_cli/cli.js
+++ totp_cli/cli.js
@@ -6,15 +6,12 @@ const hmac = createHmac('sha256', '2025');
 
 let ms = 1000 * 30;
 let timestamp = new Date(Math.round(new Date().getTime() / ms) * ms);
-console.log(timestamp);
+console.log("timestamp: " + timestamp);
 
 hmac.update(timestamp.toString());
 let numberpattern = /\d+/g;
 let result = hmac.digest('hex').match(numberpattern).join('').slice(-6);
-//let tobehashed = TOTP + timestamp;
-//let hash = createHash('sha256').update(tobehashed).digest('hex').replace(/\D/g, '').slice(null, 6);
 console.log(result);
-
 
 
 
