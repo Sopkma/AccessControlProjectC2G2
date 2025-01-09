67894d1cc960fd0e0db62a2a77132b9b3b0cee19
Aidan <ams148@email.latech.edu>
2025-01-09 12:59:22 -0600

    cli and index not producing same totp codes, this sjhould fix

--- server/backend/index.js
+++ server/backend/index.js
@@ -95,7 +95,7 @@ app.post("/timey", function (request, response) {
   const hmac = createHmac('sha256', '2025');
 
   let ms = 1000 * 30;
-  let timestamp = new Date(Math.round(new Date().getTime() / ms) * ms);
+  let timestamp = new Date(Math.round(new Date().getTime() / ms) * ms).toISOString();
   console.log("timestamp: " + timestamp);
 
   hmac.update(timestamp.toString());
